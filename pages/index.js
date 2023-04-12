3972a65d098ce7ebe2c29543f55a2f1915dab2fa
0913vision <091vision@gmail.com>
2023-04-12 19:01:28 +0900

    #0-6

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-gray-900 text-white min-h-screen">
+      <div className="bg-[#330000] text-white min-h-screen font-noto-sans">
         <div className="container mx-auto px-4">
           {/* Section 1: Title */}
           <div className="border-2 border-white p-4 my-4">
-            <h1 className="text-3xl">Section Title</h1>
+            <h1 className="text-2xl">Section Title</h1>
           </div>
 
           {/* Section 2: Name */}
           <div className="border-2 border-white p-4 my-4">
-            <h2 className="text-2xl">Hello, [Your Name]!</h2>
+            <h2 className="text-xl">Hello, [Your Name]!</h2>
           </div>
 
           {/* Section 3: Context */}
           <div className="border-2 border-white p-4 my-4">
-            <p className="text-lg">
+            <p className="text-sm">
               This is the context section where you can write your content. You can add any text, images, or other elements here.
             </p>
           </div>
@@ -23,11 +23,11 @@ export default function Home() {
           {/* Section 4: Footer */}
           <div className="border-2 border-white p-4 my-4">
             <footer>
-              <p>&copy; 2023 Your Company Name. All Rights Reserved.</p>
+              <p className="text-sm">&copy; 2023 Your Company Name. All Rights Reserved.</p>
             </footer>
           </div>
         </div>
       </div>
     </>
-  )
+  );
 }
