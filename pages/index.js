acb2fdeec635e467bbef87b93cc24df18aaeb4bf
0913vision <kim-yoochan@gim-yuchan-ui-MacBookAir.local>
2023-04-12 18:09:40 +0900

    #0-3 init

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 export default function Home() {
   return (
     <main>
-      hello
+      <div class="letter">
+      <p>Creating a messy stack of papers using the magic of CSS transforms and generated content.</p>
+
+      <p>Forked from [Michael Martin-Smucker](https://codepen.io/mlms13/)</p> <p>Pen [CSS Stacked Paper Effect](https://codepen.io/mlms13/pen/LKFoy/).</p>
+        <h3>[HOVER ME]</h3>
+      </div>
     </main>
   )
 }
